a8de2ab61b20f2a007e5f1451d27fa91f354c04c
Vlad <ricean.vlad7@gmail.com>
2023-10-15 11:21:15 +0300

    Trigger alarm dummy

--- app/src/main/java/com/riviem/sunalarm/MainActivity.kt
+++ app/src/main/java/com/riviem/sunalarm/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.riviem.sunalarm
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,6 +14,14 @@ import androidx.compose.ui.Modifier
 import com.riviem.sunalarm.features.home.presentation.HomeRoute
 import com.riviem.sunalarm.ui.theme.SunAlarmTheme
 import dagger.hilt.android.AndroidEntryPoint
+
+class AlarmReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+        println("vladlog: AlarmReceiver Alarm Triggered!")
+        Toast.makeText(context, "Alarm Triggered!", Toast.LENGTH_LONG).show()
+    }
+}
+
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
--- app/src/main/java/com/riviem/sunalarm/features/home/presentation/HomeScreen.kt
+++ app/src/main/java/com/riviem/sunalarm/features/home/presentation/HomeScreen.kt
@@ -1,13 +1,16 @@
 package com.riviem.sunalarm.features.home.presentation
 
+import android.annotation.SuppressLint
+import android.app.AlarmManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
+import android.widget.Toast
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
@@ -24,6 +27,10 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.riviem.sunalarm.AlarmReceiver
+import java.util.Calendar
+
+
 
 @Composable
 fun HomeRoute(
@@ -35,13 +42,14 @@ fun HomeRoute(
     )
 }
 
+@SuppressLint("ScheduleExactAlarm")
 @Composable
 fun HomeScreen(
     modifier: Modifier = Modifier,
     context: Context
 ) {
 
-    var hour by remember { mutableIntStateOf(0) }
+    var hour by remember { mutableIntStateOf(16) }
     var minute by remember { mutableIntStateOf(0) }
 
     Box(
@@ -53,39 +61,55 @@ fun HomeScreen(
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Button(
-                onClick = {
-
-                },
-            ) {
-                Text("Set Alarm")
-            }
+            SetAlarmButton(hour, minute, context)
             Spacer(modifier = Modifier.padding(16.dp))
-            Row(
+            TextField(
+                value = hour.toString(),
+                onValueChange = { hour = if (it.isNotEmpty()) it.toInt() else 0 },
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Number
+                ),
                 modifier = Modifier
-                    .fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceAround
-            ) {
-                TextField(
-                    value = hour.toString(),
-                    onValueChange = { hour = it.toInt() },
-                    keyboardOptions = KeyboardOptions(
-                        keyboardType = KeyboardType.Number
-                    ),
-                    modifier = Modifier
-                        .padding(16.dp)
-                )
-                TextField(
-                    value = minute.toString(),
-                    onValueChange = { minute = it.toInt() },
-                    keyboardOptions = KeyboardOptions(
-                        keyboardType = KeyboardType.Number
-                    ),
-                    modifier = Modifier
-                        .padding(16.dp)
-                )
-            }
+                    .padding(16.dp)
+            )
+            TextField(
+                value = minute.toString(),
+                onValueChange = { minute = if (it.isNotEmpty()) it.toInt() else 0 },
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Number
+                ),
+                modifier = Modifier
+                    .padding(16.dp)
+            )
         }
     }
 }
 
+@SuppressLint("ScheduleExactAlarm")
+@Composable
+private fun SetAlarmButton(hour: Int, minute: Int, context: Context) {
+    Button(onClick = {
+        // Get the AlarmManager
+        val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+
+        // Create a Calendar object for the alarm time
+        val calendar = Calendar.getInstance()
+        calendar.set(Calendar.HOUR_OF_DAY, 11)
+        calendar.set(Calendar.MINUTE, 21)
+        calendar.set(Calendar.SECOND, 0)
+
+        // Create a PendingIntent object for the alarm receiver
+        val intent = Intent(context, AlarmReceiver::class.java)
+        val pendingIntent = PendingIntent.getBroadcast(context, 0, intent,
+            PendingIntent.FLAG_IMMUTABLE)
+
+        // Set the alarm
+        alarmManager.setExact(AlarmManager.RTC_WAKEUP, calendar.timeInMillis, pendingIntent)
+
+        println("vladlog: Alarm set!")
+        Toast.makeText(context, "Alarm set!", Toast.LENGTH_SHORT).show()
+    }) {
+        Text("Set Alarm")
+    }
+
+}
