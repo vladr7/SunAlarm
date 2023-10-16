26436e99eca8f6b8e6ad848daa169109fac1a3e9
Vlad <ricean.vlad7@gmail.com>
2023-10-16 13:35:25 +0300

    Ask permission to display over other apps

--- app/src/main/java/com/riviem/sunalarm/MainActivity.kt
+++ app/src/main/java/com/riviem/sunalarm/MainActivity.kt
@@ -3,7 +3,9 @@ package com.riviem.sunalarm
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.Settings
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -34,6 +36,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    askPermissionDisplayOverOtherApps(this)
                     HomeRoute()
                 }
             }
@@ -41,3 +44,15 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+private fun askPermissionDisplayOverOtherApps(
+    context: Context
+) {
+    if (Settings.canDrawOverlays(context)) {
+        // You have the permission
+    } else {
+        // You do not have the permission. Open the settings to let user grant it.
+        val intent = Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:${context.packageName}"))
+        context.startActivity(intent)
+    }
+
+}
