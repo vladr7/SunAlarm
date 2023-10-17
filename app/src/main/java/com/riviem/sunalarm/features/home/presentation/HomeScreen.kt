f4673da0ad7f4cb8d98b672db04c1b41fdaa4e7b
Vlad <ricean.vlad7@gmail.com>
2023-10-17 09:56:05 +0300

    Home screen UI wip

--- app/src/main/java/com/riviem/sunalarm/features/home/presentation/HomeScreen.kt
+++ app/src/main/java/com/riviem/sunalarm/features/home/presentation/HomeScreen.kt
@@ -1,35 +1,24 @@
 package com.riviem.sunalarm.features.home.presentation
 
 import android.annotation.SuppressLint
-import android.app.AlarmManager
-import android.app.PendingIntent
 import android.content.Context
-import android.content.Intent
-import android.widget.Toast
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Text
-import androidx.compose.material3.TimeInput
-import androidx.compose.material3.TimePickerState
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
-import com.riviem.sunalarm.AlarmReceiver
 import com.riviem.sunalarm.MainActivity
-import java.util.Calendar
 
 
 @Composable
@@ -42,7 +31,6 @@ fun HomeRoute(
     )
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
 @SuppressLint("ScheduleExactAlarm")
 @Composable
 fun HomeScreen(
@@ -50,17 +38,6 @@ fun HomeScreen(
     context: Context,
 ) {
     val activity = context as MainActivity
-
-    var paginaDePornire: String by remember {
-        mutableStateOf("PaginaDePornire")
-    }
-    val timePickerState = remember {
-        TimePickerState(
-            initialHour = 16,
-            initialMinute = 10,
-            is24Hour = true
-        )
-    }
 
     Box(
         modifier = modifier
@@ -71,48 +48,38 @@ fun HomeScreen(
             verticalArrangement = Arrangement.Top,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Spacer(modifier = Modifier.padding(16.dp))
-            SetAlarmButton(hour = timePickerState.hour, minute = timePickerState.minute, context)
-            Spacer(modifier = Modifier.padding(16.dp))
-            TimeInput(
-                state = timePickerState
+            HomeScreenTitle(
+                modifier = modifier
+                    .padding(top = 100.dp),
             )
-            Button(onClick = {
-                activity.finishAffinity()
-            }) {
-                Text("Cancel Alarm")
-            }
         }
     }
 }
 
-@SuppressLint("ScheduleExactAlarm")
 @Composable
-private fun SetAlarmButton(hour: Int, minute: Int, context: Context) {
-    Button(onClick = {
-        // Get the AlarmManager
-        val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
-
-        // Create a Calendar object for the alarm time
-        val calendar = Calendar.getInstance()
-        calendar.set(Calendar.HOUR_OF_DAY, hour)
-        calendar.set(Calendar.MINUTE, minute)
-        calendar.set(Calendar.SECOND, 0)
-
-        // Create a PendingIntent object for the alarm receiver
-        val intent = Intent(context, AlarmReceiver::class.java)
-        val pendingIntent = PendingIntent.getBroadcast(
-            context, 0, intent,
-            PendingIntent.FLAG_IMMUTABLE
+fun HomeScreenTitle(
+    modifier: Modifier = Modifier,
+    title: String = "Next alarm in 6 hours 1 minute",
+    subtitle: String = "Tue, Oct 17, 15:29",
+) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = title,
+            fontSize = 30.sp,
+            color = Color.White,
+            textAlign = TextAlign.Center
         )
-
-        // Set the alarm
-        alarmManager.setExact(AlarmManager.RTC_WAKEUP, calendar.timeInMillis, pendingIntent)
-
-        println("vladlog: Alarm set! $hour:$minute")
-        Toast.makeText(context, "Alarm set!", Toast.LENGTH_SHORT).show()
-    }) {
-        Text("Set Alarm")
+        Spacer(modifier = Modifier.padding(4.dp))
+        Text(
+            text = subtitle,
+            fontSize = 16.sp,
+            color = Color.White,
+            textAlign = TextAlign.Center
+        )
     }
-
 }
+
