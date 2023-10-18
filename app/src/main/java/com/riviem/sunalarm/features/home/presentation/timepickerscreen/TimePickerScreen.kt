be8fa1cc71fa4c793f7ad322861649b6e3c5d59f
Vlad <ricean.vlad7@gmail.com>
2023-10-18 07:20:01 +0300

    Increase tap area

--- app/src/main/java/com/riviem/sunalarm/features/home/presentation/timepickerscreen/TimePickerScreen.kt
+++ app/src/main/java/com/riviem/sunalarm/features/home/presentation/timepickerscreen/TimePickerScreen.kt
@@ -5,10 +5,11 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.material.Text
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
@@ -48,36 +49,51 @@ fun ScrollableTimePicker(
         verticalAlignment = Alignment.CenterVertically
     ) {
         LazyColumn(
+            modifier = Modifier
+                .weight(1f),
             content = {
                 items(hours) { hour ->
-                    Text(
-                        text = hour,
-                        color = Color.White,
-                        fontSize = 50.sp,
-                        modifier = Modifier
-                            .padding(bottom = 30.dp)
+                    TimeScrollItem(
+                        time = hour,
+                        modifier = Modifier.fillMaxWidth()
                     )
                 }
             },
             state = hoursState
         )
-        Text(text = ":", color = Color.White, fontSize = 50.sp)
+        Text(
+            text = ":",
+            color = Color.White,
+            fontSize = 50.sp,
+            modifier = Modifier.padding(5.dp)
+        )
         LazyColumn(
+            modifier = Modifier
+                .weight(1f),
             content = {
                 items(minutes) { minute ->
-                    Text(
-                        text = minute,
-                        color = Color.White,
-                        fontSize = 50.sp,
-                        modifier = Modifier
-                            .padding(bottom = 30.dp)
+                    TimeScrollItem(
+                        time = minute,
+                        modifier = Modifier.fillMaxWidth()
                     )
                 }
-
             },
             state = minutesState
         )
     }
 }
 
-
+@Composable
+private fun TimeScrollItem(
+    modifier: Modifier = Modifier,
+    time: String
+) {
+    Text(
+        text = time,
+        color = Color.White,
+        fontSize = 50.sp,
+        modifier = modifier
+            .padding(bottom = 30.dp)
+            .wrapContentSize(Alignment.Center)
+    )
+}
