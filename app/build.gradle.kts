c4315f31b790b56e4583dbc78fc2e0663dc317f0
Vlad <ricean.vlad7@gmail.com>
2023-12-01 15:05:31 +0200

    Update version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         applicationId = "com.riviem.sunalarm"
         minSdk = 27
         targetSdk = 34
-        versionCode = 2
+        versionCode = 3
         versionName = "0.9.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
