bccbeba92b41b2a2c46b9c3cb1a775969a7e2d90
Vlad <ricean.vlad7@gmail.com>
2024-02-23 09:09:59 +0200

    Update version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.riviem.sunalarm"
         minSdk = 27
         targetSdk = 34
-        versionCode = 4
-        versionName = "0.9.1"
+        versionCode = 5
+        versionName = "0.9.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
